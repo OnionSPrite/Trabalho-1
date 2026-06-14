8db1eefb33c35671d71202b77b7b07c372b9d4bf
jeanpovaluk-code <jeanpovaluk@gmail.com>
2026-06-14 13:13:13 -0300

    Mudanças no código
    adcionado algumas funções, mudança no uso da struct e implementado a base para o uso de 'switch'

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #define MAX 100
 
-//Horas gastas no código: 4 (Atualize sempre que mexer no código :D);
+//Horas gastas no código: 5 (Atualize sempre que mexer no código :D);
 //NÃO ESQUECER O PONTO E VÍRGULA (;);
 //NÃO ESQUECER O 'E UNIVERSAL (&)' NO SCANF;
 //Conferir SEMPRE as chaves ({});
@@ -10,11 +10,29 @@
 //O Codigo deve apresentar Erro nas ocasiões onde o Conjunto selecionado não existe.
 //Um conjunto que não há numeros dentro mas foi declarado, ainda é um conjunto que EXISTE.
 
-struct leitura_de_cmd { // Struct que define o comando executado dentro da nossa calculadora, o conjunto selecionado e o valor inserido;
-    char interacao[1];
-    int conjunto;
-    int valor;
-};
+typedef struct { // Struct que define os conjuntos
+    int *elementos;
+    int tamanho;
+    int capacidade;
+}Conjunto;
+
+//inicia um conjunto vazio
+Conjunto* cria_conj(int capacidadeInicial){
+    Conjunto *c = (Conjunto*)malloc(sizeof(Conjunto));
+    if(c == NULL){
+        printf("Erro de alocação de memória!");
+        exit(1);
+    }
+    c->elementos = (int*)malloc(capacidadeInicial * sizeof(int));
+        if(c->elementos == NULL){
+            printf("Erro de alocação de memória!");
+            free(c);
+            exit(1);
+        }
+    c->tamanho = 0;
+    c->capacidade = capacidadeInicial;
+    return c;
+}
 
 void imprime_conjuntos (int m[MAX][MAX], int col[MAX], int n) { //Funcao que faz a impressao dos conjuntos, do 1 ao ultimo;
     for (int i = 0; i < n; i++) {
@@ -57,6 +75,60 @@ int unir_conj (int *con1, int *con2){
     }
     int aux[MAX];
     int z = 0;
+    int x = 0;
+    int *con3;
+    *con3 = *con1;
+    for (int i = 0; i < maior_conjunto; i++){
+        if(con1[i] != con2[i]){
+            aux[z] = con2[i];
+            z++;
+        }
+    }
+
+    int tamanho_conj3 = maior_conjunto + z;
+    for (int i = 0; i < tamanho_conj3; i++){
+    if (con3[i] == NULL){
+    con3[i] = aux[x]
+    x++
+       }
+    }
+
+    return *con3;
+}
+
+int intercept_conj (int *con1, int *con2){
+    int tamanho_conj1 = sizeof(con1)/sizeof(con1[0]);
+    int tamanho_conj2 = sizeof(con2)/sizeof(con2[0]);
+    int maior_conjunto;
+    if(tamanho_conj1 > tamanho_conj2){
+        maior_conjunto = tamanho_conj1;
+    }else{
+        maior_conjunto = tamanho_conj2;
+    }
+    int z = 0;
+    int x = 0;
+    int *con3;
+       for (int i = 0; i < maior_conjunto; i++){
+        if(con1[i] = con2[i]){
+            con3[z] = con2[i];
+            z++;
+        }
+    }
+
+    return *con3;
+}
+
+int unir_conj (int *con1, int *con2){
+    int tamanho_conj1 = sizeof(con1)/sizeof(con1[0]);
+    int tamanho_conj2 = sizeof(con2)/sizeof(con2[0]);
+    int maior_conjunto;
+    if(tamanho_conj1 > tamanho_conj2){
+        maior_conjunto = tamanho_conj1;
+    }else{
+        maior_conjunto = tamanho_conj2;
+    }
+    int aux[MAX];
+    int z = 0;
     int *con3;
     *con3 = *con1;
     for (int i = 0; i < maior_conjunto; i++){
@@ -74,7 +146,6 @@ int unir_conj (int *con1, int *con2){
 
 int main(){
     int n, c, *col, **m, num_conjuntos; // Definicao das variaveis
-    struct leitura_de_cmd comando;
     scanf("%d", &n); // Quantidade inicial de conjuntos
     c = (int)malloc(n * sizeof(int));
     m = (int **)malloc(n * sizeof(int *));
@@ -88,9 +159,31 @@ int main(){
         }
     }
 
-    while (comando.interacao[0] != "*") { // Calculadora SUPER CALCULADORA de conjuntos SUPER CONJUNTOS;
-        scanf("%s %d %d", comando.interacao[0], &comando.conjunto, &comando.valor); // Recebe do usuario o que sera feito em sequencia;
+char comando;
+while(1){
+    scanf("%c", &comando);
+    if(comando == '*'){
+        break;
     }
+    switch (comando) { //switch que recebe qual interação o usuário vai fazer na calculadora
+        case '+':{
+            int i, j;
+        }
+
+        case 'u':{
+            int i, j;
+        }
+
+        case 'i':{
+            int i, j;
+        }
+
+        case '-':{
+            int i, j;
+        }
+    }
+
+}
 
     for (int i = 0; i < n; i++) {
         free(m[i]);
